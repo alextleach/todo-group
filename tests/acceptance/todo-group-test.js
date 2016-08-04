963457bb0cd14cf252925405e2685fcaf2584e8b
Alex T Leach <alextleach@gmail.com>
2016-08-03 21:39:40 -0500

    getting a test error that i'm confused by

--- tests/acceptance/todo-group-test.js
+++ tests/acceptance/todo-group-test.js
@@ -13,11 +13,21 @@ test('visiting /todo-group', function(assert) {
 
 test('visting /todo-groups shows a list of tasks', function(assert){
   server.createList('todo-group', 10);
-  visit('todo-groups')
+  visit('todo-groups');
 
   andThen(function() {
     findWithAssert('.todo-list');
     assert.equal(findWithAssert('.todo-list__item').length, 10);
-  })
+  });
 
-})
+});
+
+test('user can navigate from main list to new todo list', function(assert) {
+  visit('/todo-groups');
+  click('.new-link');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/todo-groups/new');
+    assert.equal(findWithAssert('.page-title').text().trim(), 'Create a New Todo List!');
+  });
+});
