9ddf4081705cae56359060811d62988e953c5849
Alex T Leach <alextleach@gmail.com>
2016-08-08 14:59:01 -0500

    working on outlet tests and getting mirage models imported

--- mirage/factories/todo-item.js
+++ mirage/factories/todo-item.js
@@ -1,7 +1,7 @@
 import { Factory, faker } from 'ember-cli-mirage';
 
 export default Factory.extend({
-  title() {
-    return faker.commerce.product();
+  name() {
+    return faker.random.word();
   }
 });
