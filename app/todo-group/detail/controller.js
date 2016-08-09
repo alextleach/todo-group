5647e698d911ca5e3ce57a4a836780f5cb4e0827
Alex T Leach <alextleach@gmail.com>
2016-08-09 17:20:26 -0500

    got all the tests to pass

--- app/todo-group/detail/controller.js
+++ app/todo-group/detail/controller.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    destroy(todoItem) {
+      todoItem.destroyRecord();
+    },
+
+  toggleCheck(todoItem) {
+    todoItem.toggleProperty('done');
+    todoItem.save();
+  },
+
+    createTodoItem(todoGroup, formValues, reset){
+      const storeItem = this.store.createRecord('todo-item', formValues);
+      storeItem.set('group', todoGroup);
+      storeItem.save();
+      reset();
+    },
+  }
 });
